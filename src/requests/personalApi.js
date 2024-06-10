580a6ae739eea5ca1d2f56f5347276b44dbf92fd
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-10 15:38:01 -0300

    feat: create request to verify auth at fragment times

--- src/requests/personalApi.js
+++ src/requests/personalApi.js
@@ -1,8 +1,22 @@
 import api from "./api"; 
 
-export async function auth() { 
+export async function auth(personalData) { 
     try {
-        const response = await api.post('/auth');
+        const response = await api.post('/auth', personalData);
+        return response.data;
+    } catch (error) {
+        console.error('Error retrieving techs:', error);
+        return [];
+    }
+}
+
+export async function verifyAuth(token) { 
+    try {
+        const config = {
+            headers: { Authorization: `Bearer ${token}` },
+        };
+
+        const response = await api.post('/verify/auth', null, token);
         return response.data;
     } catch (error) {
         console.error('Error retrieving techs:', error);
