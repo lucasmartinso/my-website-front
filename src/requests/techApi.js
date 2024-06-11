0e5a2f5bbe08c29aba8efc9bbd7e23df1739d147
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-11 10:44:49 -0300

    feat: create update tech request

--- src/requests/techApi.js
+++ src/requests/techApi.js
@@ -11,12 +11,27 @@ export async function getTechs() {
 }
 
 export async function searchTechs(tech) {
-    console.log(tech); 
     try {
         const response = await api.post('/techs/search',tech);
         return response.data;
     } catch (error) {
         console.log('Error retrieving techs:', error); 
         return [];
+    }
+}
+
+export async function deleteTech(id,config) {
+    try {
+        await api.delete(`/delete/techs/${id}`,null,config);
+    } catch (error) {
+        console.log('Error deleting techs:', error); 
+    }
+}
+
+export async function deleteTech(id,techData,config) {
+    try {
+        await api.put(`/edit/techs/${id}`,techData,config);
+    } catch (error) {
+        console.log('Error updating techs:', error); 
     }
 }
