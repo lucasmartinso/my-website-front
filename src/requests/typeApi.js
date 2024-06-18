6a944a37f936481a37f2bda96b4befd49abba23c
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-18 19:18:36 -0300

    feat: create request to update and delete type

--- src/requests/typeApi.js
+++ src/requests/typeApi.js
@@ -13,3 +13,11 @@ export async function getTypes() {
 export async function postType(data,config) { 
     await api.post('/types',data,config);
 }
+
+export async function deleteType(id,config) { 
+    await api.post(`/delete/types/${id}`,config);
+}
+
+export async function updateType(id,data,config) { 
+    await api.put(`/edit/types/${id}`,data,config);
+}
