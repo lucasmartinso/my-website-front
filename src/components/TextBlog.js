722ef346f9cfc914001d298d3f7367d76764059f
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-03 14:59:30 -0300

    feat: tittle construct

--- src/components/TextBlog.js
+++ src/components/TextBlog.js
@@ -1,20 +1,69 @@
 import styled from "styled-components";
+import books from "../styles/images/books.gif";
+import { useNavigate } from "react-router-dom";
 
 export default function TextBlogScreen() { 
+    const navigate = useNavigate();
+    
     return(
         <Tittle>
-            <p>Titulo</p>
-            <img src="" alt="books"/>
-            <span>Description</span>
+            <p>Lorem Ipsum njjcjdcnjsdn JKAHAHAHA</p>
+            <img src={books} alt="books"/>
+            <span>Lorem Ipsum njjcjdcnjsdn JKAHAHAHA Lorem Ipsum njjcjdcnjsdn JKAHAHAHA Lorem Ipsum njjcjdcnjsdn JKAHAHAHA Lorem Ipsum njjcjdcnjsdn JKAHAHAHA</span>
+            <ion-icon name="arrow-back" onClick={() => navigate("/hello")}></ion-icon>
         </Tittle>
     )
 }
 
 const Tittle = styled.div`
-    width: 100%; 
-    height: 200px;
+    margin-left: -30px;
+    margin-right: -30px;
+    width: calc(100% + 60px);
+    height: 300px;
     display: flex; 
+    flex-direction: column;
     justify-content: center; 
     align-items: center; 
-    background-color: wine;
+    background-color: blue;
+    border-radius: 0px 0px 20px 20px;
+    box-shadow: rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px, rgba(0, 0, 0, 0.09) 0px 64px 32px;
+    position: relative;
+
+    p { 
+        width: 50%;
+        text-align: center;
+        font-size: 30px; 
+        font-weight: bold;
+    }
+
+    span { 
+        width: 70%;
+        font-weight: light;
+        text-align: center;
+        word-break: normal;
+        white-space: normal;
+        overflow-wrap: break-word;
+        text-overflow: ellipsis;
+        hyphens: auto;
+    }
+
+    img { 
+        object-fit: cover;
+        width: 100px; 
+        height: 100px;
+    }
+
+    ion-icon { 
+        position: absolute;
+        width: 40px; 
+        height: 40px;
+        left: 20px; 
+        top: 20px;
+
+        &:hover, 
+        &:focus { 
+            cursor: pointer; 
+            color: white;
+        }
+    }
 `
