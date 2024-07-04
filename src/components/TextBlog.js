2199533ff33b5cc38e4745027a6a3061dcb5a131
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-04 17:33:29 -0300

    feat: get text of blog

--- src/components/TextBlog.js
+++ src/components/TextBlog.js
@@ -4,31 +4,17 @@ import { useNavigate } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import BaseBoard from "../pages/BaseBoard";
 import ToggleContext from "../contexts/ToggleContext";
+import * as blogApi from "../requests/blogApi";
 
-export default function TextBlogScreen() { 
+export default function TextBlogScreen({id}) { 
     const { toggleLight } = useContext(ToggleContext);
     const [ blog, setBlog ] = useState([]);
     const navigate = useNavigate();
-    const text = `
-        <span>Introdução</span>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget magna a arcu euismod vehicula. Vivamus lacinia leo nec magna pretium, sed euismod ligula ultricies. Ut et semper nisi. In ac malesuada ligula. Maecenas ultricies lacus id diam fermentum, ut cursus est dapibus. Vivamus nec velit vel turpis fermentum aliquet a eu lacus.</p>
-        <img src="https://img.elo7.com.br/product/zoom/2FA8C62/adesivo-decorativo-parede-fone-som-e-musica-quarto.jpg" alt="Placeholder Image 1"/>
-        <p>Praesent facilisis tortor nec libero ultricies, a ullamcorper nisl faucibus. Nulla facilisi. Phasellus dictum orci ac nisi tempor, at hendrerit lacus dapibus. Integer non turpis urna. Ut eget quam urna. Nulla non magna id libero fermentum suscipit. Donec tincidunt arcu a felis sodales, a hendrerit felis condimentum.</p>
-        <p>Mauris efficitur metus vitae nulla vestibulum feugiat. Phasellus nec magna quis libero ultrices vulputate. Vivamus dignissim orci a velit placerat, at egestas erat vehicula. Suspendisse euismod nisl quis magna mollis, eu efficitur risus efficitur. Suspendisse potenti.</p>
-        <span>E ai será?</span>
-        <p>Sed vitae lorem et lorem sodales tempus. Integer convallis nulla ut orci scelerisque, at varius leo dictum. Quisque suscipit metus in sapien volutpat, ut auctor mauris malesuada. Morbi nec urna vitae nunc fermentum placerat. Duis convallis quam vel ante malesuada, at sollicitudin purus sagittis.</p>
-        <p>Phasellus feugiat sapien et bibendum tincidunt. Proin sed dui a nisi accumsan tempus. Curabitur efficitur risus vel elit dictum, nec consequat tortor consectetur. Proin pharetra, turpis eget convallis elementum, odio sem ultrices lectus, eget fringilla neque erat sit amet lectus.</p>
-        <p>Aliquam erat volutpat. Integer consectetur ipsum sit amet odio elementum cursus. Vestibulum eu turpis nec velit vehicula scelerisque. Duis tempus enim at tincidunt tempus. Nullam dictum libero vitae dui consectetur, at facilisis lorem sodales. Nulla euismod, ex eget mollis faucibus, urna nunc sagittis magna, nec dignissim leo dui in dui.</p>
-        <p>Vestibulum quis ligula eros. Aenean ut feugiat arcu. Curabitur sed risus velit. Quisque id sapien sem. Proin quis sapien et urna sollicitudin porttitor sit amet in dui. Proin nec arcu vitae tortor cursus facilisis. Suspendisse potenti. Aliquam erat volutpat. Phasellus blandit lacus sed justo cursus, non malesuada libero pellentesque.</p>
-        <p>Donec quis magna ac quam tempus varius. Vivamus ultricies velit id erat interdum, ut vestibulum justo tincidunt. Donec vel nulla bibendum, gravida sapien ut, porttitor neque. Proin rutrum felis orci, at consectetur nisl pellentesque vel. Vivamus scelerisque, augue sed vehicula dapibus, ipsum libero aliquam felis, eget congue sapien felis id sapien.</p>
-        <p>Morbi non augue sit amet odio accumsan aliquam. Sed varius arcu nec enim tincidunt convallis. Fusce id fermentum lacus, at scelerisque metus. Duis sollicitudin nisi et lectus facilisis feugiat. Donec blandit felis vitae lectus ultrices gravida. Ut accumsan orci ac augue fermentum, in vulputate magna ullamcorper.</p>
-        <img src="https://img.elo7.com.br/product/zoom/2FA8C62/adesivo-decorativo-parede-fone-som-e-musica-quarto.jpg" alt="Placeholder Image 9"/>
-        <p>Nam vulputate massa vel venenatis porttitor. Nullam sagittis euismod nisl, sit amet laoreet risus malesuada in. Vivamus gravida ligula sed ligula posuere, in ornare libero vestibulum. Donec consequat lacus id mauris sodales, et malesuada libero pulvinar. Phasellus sed urna eget magna fermentum pretium. In consectetur odio id risus pellentesque, ac tempor lectus pulvinar.</p>
-    `
 
     useEffect(() => { 
         async function fecthBlogData() { 
-
+            const response = await blogApi.getCompleteBlog(14); 
+            setBlog(response);
         } 
 
         fecthBlogData();
@@ -37,13 +23,13 @@ export default function TextBlogScreen() {
     return(
         <Container>
             <Tittle toggleLight={toggleLight}>
-                <p>Lorem Ipsum njjcjdcnjsdn JKAHAHAHA</p>
+                <p>{blog[0].tittle}</p>
                 <img src={books} alt="books"/>
-                <span>Lorem Ipsum njjcjdcnjsdn JKAHAHAHA Lorem Ipsum njjcjdcnjsdn JKAHAHAHA Lorem Ipsum njjcjdcnjsdn JKAHAHAHA Lorem Ipsum njjcjdcnjsdn JKAHAHAHA</span>
+                <span>{blog[0].description}</span>
                 <ion-icon name="arrow-back" onClick={() => navigate("/hello")}></ion-icon>
             </Tittle>
 
-            <Content toggleLight={toggleLight} dangerouslySetInnerHTML={{ __html: text }}></Content>
+            <Content toggleLight={toggleLight} dangerouslySetInnerHTML={{ __html: blog[0].text }}></Content>
         
             <BaseBoard />
         </Container>
@@ -211,7 +197,7 @@ const Content = styled.div`
         }
     }
 
-    @media (max-width: 500px) { 
+    @media (max-width: 600px) { 
         width: 100%;
         
         img { 
