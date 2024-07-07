38d9014cfac4b256028221f3d9f350f2f9d11b3c
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-07 12:10:08 -0300

    feat: create blog page

---
+++ src/components/Blogs.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export default function BlogsScreen() { 
+    return( 
+        <Container>
+
+        </Container>
+    )
+} 
+
+const Container = styled.div`
+    width: 100%; 
+    height: auto; 
+    display: flex; 
+    justify-content: center;
+`
